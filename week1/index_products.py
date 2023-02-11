3b000f7d661a612602151733c74bf6982ce5d063
vkoolkatz <kbdesai4@gmail.com>
2023-02-11 22:48:34 +0000

    index products

--- week1/index_products.py
+++ week1/index_products.py
@@ -85,7 +85,15 @@ def get_opensearch():
     port = 9200
     auth = ('admin', 'admin')
     #### Step 2.a: Create a connection to OpenSearch
-    client = None
+    client = OpenSearch(
+                hosts=[{'host': host, 'port': port}],
+                http_compress=True,
+                http_auth=auth,
+                use_ssl=True,
+                verify_certs=False,
+                ssl_assert_hostname=False,
+                ssl_show_warn=False,
+    )
     return client
 
 
@@ -97,6 +105,7 @@ def index_file(file, index_name):
     root = tree.getroot()
     children = root.findall("./product")
     docs = []
+    client = get_opensearch()
     for child in children:
         doc = {}
         for idx in range(0, len(mappings), 2):
@@ -107,8 +116,19 @@ def index_file(file, index_name):
         if 'productId' not in doc or len(doc['productId']) == 0:
             continue
         #### Step 2.b: Create a valid OpenSearch Doc and bulk index 2000 docs at a time
-        the_doc = None
+        the_doc = {
+            '_index' : index_name,
+            '_source': doc
+        }
         docs.append(the_doc)
+        if len(docs) == 2000:
+            bulk(client, docs)
+            docs_indexed += len(docs)
+            docs.clear()
+    if len(docs) > 0:
+        bulk(client, docs)
+        docs_indexed += len(docs)
+        docs.clear()
 
     return docs_indexed
 
